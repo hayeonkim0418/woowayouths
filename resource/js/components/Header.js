1ca9cb56afd58b30d165cd6003ce80fe27f69014
hayeonkim0418 <72956163+hayeonkim0418@users.noreply.github.com>
2025-09-07 13:12:42 +0900

    ../

--- resource/js/components/Header.js
+++ resource/js/components/Header.js
@@ -2,7 +2,7 @@ import { GNB } from "./GNB.js";
 import { IncludeHTML } from "./IncludeHTML.js";
 
 export const Header = () => {
-  IncludeHTML("../components/header.html", ".header").then(() => {
+  IncludeHTML("../../components/header.html", ".header").then(() => {
     GNB();
   });
 };
